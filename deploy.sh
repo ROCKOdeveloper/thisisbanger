881e387a79d42ee84db5619277f5025ddacd4fff
ROCKOraboloco <114123625+ROCKOdeveloper@users.noreply.github.com>
2022-11-06 14:28:45 -0500

    7

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ROCKOdeveloper/thisisbanger.git main:gh-pages
+git push -f git@github.com:ROCKOdeveloper.github.io/thisisbanger.git main:gh-pages
 
 cd -
